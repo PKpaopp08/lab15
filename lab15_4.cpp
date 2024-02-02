adfd37d4ba9c8c1a18efc4efd41c357e919541ad
PAO <natthachai_bubpa@cmu.ac.th>
2024-02-02 14:24:14 +0700

    123

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,17 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b, int &c, int &d)
+{
+	int A[]={a,b,c,d};
+	int B,C;
+	for(int i=0;i<4;i++)
+	{
+		B=A[i];
+		C=rand()%4;
+		A[i]=A[C];
+		A[C]=B;
+	}
+	a=A[0];b=A[1];c=A[2];d=A[3];
+}
